856fd1c3f0eccb6acaa55ac7b7db491b1dc20ae9
AleksiKlimov <leha.kurdyukov@internet.ru>
2022-10-13 16:24:53 +0300

    finished the desktop versiion

---
+++ index.js
@@ -0,0 +1,13 @@
+<div class="photo-section__column-images-2"></div>
+<!-- third 3 picture--------------------------------------->
+<div class="photo-section__column-images-3"></div>
+<!-- fourth 4 picture--------------------------------------->
+<div class="photo-section__column-images-4"></div>
+<!-- fiveth 5 picture--------------------------------------->
+<div class="photo-section__column-images-5"></div>
+<!-- sixth 6 picture--------------------------------------->
+<div class="photo-section__column-images-6"></div>
+<!-- seventh 7 picture--------------------------------------->
+<div class="photo-section__column-images-7"></div>
+<!-- eith 8 picture--------------------------------------->
+<div class="photo-section__column-images-8"></div>
